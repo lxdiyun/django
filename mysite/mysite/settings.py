f2e027032568dfac1e2704707663f6ab8bccfcc7
lxdiyun <lxdiyun@gmail.com>
2013-03-06 20:18:08 +0800

    add daterane filter

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -127,6 +127,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
     'grappelli',
+    'daterange_filter',
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
