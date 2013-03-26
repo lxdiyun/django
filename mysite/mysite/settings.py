8d9e25686305fcb0a153a8a57fa78c9c37ab1166
Adrain Lee <lxdiyun@gmail.com>
2013-03-27 07:57:57 +0800

    change prefix url to website

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -56,7 +56,7 @@ MEDIA_ROOT = '/home/data/mysite/media/'
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = '/mysite/media/'
+MEDIA_URL = '/website/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -66,7 +66,7 @@ STATIC_ROOT = '/home/data/mysite/static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/mysite/static/'
+STATIC_URL = '/website/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
